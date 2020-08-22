05c59cf1b3d8db4c00aaa4de445c97fe7ea27b10
Mustafaa909 <49893240+Mustafaa909@users.noreply.github.com>
2020-08-21 18:25:43 -0700

    Update ui.R

--- ui.R
+++ ui.R
@@ -1,46 +1,73 @@
-library(DT)
-library(shiny)
-library(shinydashboard)
-
-dashboardPage(
-    skin = "green",
-    dashboardHeader(title = "Swiftkey"),
-    dashboardSidebar(id="",
-                     sidebarMenu(
-                         menuItem("Next Word Prediction", tabName = "main", icon = icon("keyboard")),
-                         menuItem("Documentation", tabName = "extra", icon = icon("book"))
-                     ) ),
-    
-    dashboardBody(
-        tabItems(
-            tabItem(
-                tabName = "main", h1("Next Word Prediction"),
-                
-                fluidRow(
-                    box(title="Type in word(s) below:",
-                        textAreaInput(inputId="text", label= NULL, width="200px", value=""), status = "success", solidHeader = TRUE,),
-                    
-                    valueBoxOutput("topPrediction", width=6) ),
-                
-                fluidRow( 
-                    box(
-                        title = "Possible next words",
-                        collapsible = TRUE, status = "success", solidHeader = TRUE,
-                        div(DTOutput('table'), style = "height: 300px")),
-                    
-                    box(
-                        title = "Word cloud",
-                        collapsible = TRUE, status = "success", solidHeader = TRUE,
-                        plotOutput("plot", height = "300px"))
-                )),
-            #tab item 2
-            tabItem(
-                tabName = "extra", h1("Documentation"),
-                fluidRow(
-                    box(width=12,
-                        h4("This application serves as the final product for a Data Science project as part of the challenge involved figuring out how to work with a new type of data to develop a working prediction algorithm and ultimately a useful data product.", 
-                           tags$br(), tags$br(), 
-                           "Based on an N-Gram Language Model, the prediction algorithm developed provides fitting suggestions to allow for words and thoughts going forward.",              tags$br(), tags$br(),  
-                           "The source code along with further information on the project can be found here:", tags$a("Source Code", href= "https://github.com/wowkazmir/Data-Science-Capstone"), style = "font-size: 125%"))))
-        ))
+suppressWarnings(library(shiny))
+suppressWarnings(library(markdown))
+suppressWarnings(library(shinyBS))
+suppressWarnings(library(shinythemes))
+shinyUI(navbarPage("Text Mining Project", theme = shinytheme("superhero"),
+                   tabPanel("Word Prediction",
+                            sidebarLayout(
+                                sidebarPanel(
+                                    helpText("The user should enter a sequence of words"),
+                                    textInput("inputString", "Write here",value = "")
+                                ),
+                                mainPanel(
+                                    h2("Prediction"),
+                                    verbatimTextOutput("prediction"),
+                                    strong("User's sequence"),
+                                    tags$style(type='text/css', '#answer {background-color:color: white;}'), 
+                                    textOutput('answer'),
+                                    br(),
+                                    strong("Which dataset has been used?"),
+                                    tags$style(type='text/css', '#method {background-color:color: white;}'),
+                                    textOutput('method')
+                                )
+                            )
+                   ),
+                   
+                   tabPanel(
+                       
+                       "Help",
+                       
+                       p("A Shiny app has been developped to take as input a phrase (multiple words) in a text box input and to output a prediction of the next word."),
+                       
+                       tags$div("Please find attached the method to predict the next word:",
+                                tags$ul(
+                                    tags$li(strong("First,"), "build bigram.RData, trigram.RData and quadgram.RData are obtained with the milestone report"),
+                                    tags$li(strong("Then,"), "quadgram.RData is used if the last used 3 words are in the dataset"),
+                                    tags$li(strong("Else,"), "trigram.RData is used if the last used 2 words are in the dataset"),
+                                    tags$li(strong("Else,"), "bigram.RData is used if the last used word is in the dataset"),
+                                    tags$li(strong("Else,"), "the word with the highest frequency is returned"),
+                                    tags$li(strong("Finally,", "here is your prediction"))
+                                ),
+                                
+                                tableOutput("dataStructure")
+                                
+                       )
+                       
+                   ),
+                   
+                   
+                   
+                   tabPanel(
+                       
+                       "About",
+                       
+                       h3("Data Science project"),
+                       
+                       h3("Author: Mustafa Houd - 24th of August, 2020"),
+                       
+                       br(),
+                       
+                       p("This application is a web application using R and ",
+                         a(href = "https://shiny.rstudio.com/", "Shiny library"),
+                         ),
+                       
+                       p("Source code is available at",
+                         a(href = "https://github.com/maximeverges/Data-Science-Capstone",
+                           "https://github.com/maximeverges/Data-Science-Capstone")
+                       )
+                       
+                   )
+                   
+                   
+)
 )
